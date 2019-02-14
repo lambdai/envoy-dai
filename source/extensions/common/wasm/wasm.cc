0006396e535c2fa58d86134187b1bc64618c98f9
Yuchen Dai <silentdai@gmail.com>
2019-02-14 00:39:32 -0800

    micro refactor, cont

--- source/extensions/common/wasm/wasm.cc
+++ source/extensions/common/wasm/wasm.cc
@@ -1017,7 +1017,7 @@ void Wasm::getFunctions() {
 #undef _GET
 }
 
-Wasm::Wasm(const Wasm& wasm) {
+Wasm::Wasm(const Wasm& wasm) : std::enable_shared_from_this<Wasm>() {
   wasm_vm_ = wasm.wasmVm()->clone();
   general_context_ = createContext();
   getFunctions();
