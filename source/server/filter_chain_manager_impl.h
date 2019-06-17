c7101e48b580713e0928860ed91442e9989a7ffb
Yuchen Dai <silentdai@gmail.com>
2019-06-17 18:25:04 +0000

    proto fix
    Signed-off-by: Yuchen Dai <silentdai@gmail.com>

--- source/server/filter_chain_manager_impl.h
+++ source/server/filter_chain_manager_impl.h
@@ -141,16 +141,13 @@ private:
   // Mapping of FilterChain's configured destination ports, IPs, server names, transport protocols
   // and application protocols, using structures defined above.
   DestinationPortsMap destination_ports_map_;
-<<<<<<< HEAD
   Network::Address::InstanceConstSharedPtr address_;
   ProtobufMessage::ValidationVisitor& validation_visitor_;
-=======
   // FilterChainList active_filter_chains_;
   // FilterChainList warming_filter_chains_;
   // std::list<DrainingFilterChain> draining_filter_chain_;
   // ConfigTracker::EntryOwnerPtr config_tracker_entry_;
   // FcdsApiPtr fcds_api_;
->>>>>>> 2baecd0f7... fcds: proto and build
 };
 
 class FilterChainImpl : public Network::FilterChain {
